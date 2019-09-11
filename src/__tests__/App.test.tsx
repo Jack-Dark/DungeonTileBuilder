09c14ff356f12fee358e886bdee6ebe75e992059
Jack-Dark <decker3d@gmail.com>
2019-09-10 21:56:36 -0500

    refactor: Adds wrapper type and renames var.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import App from "../App";
-import { shallow, mount, render } from "enzyme";
+import { shallow, mount, render, ShallowWrapper } from "enzyme";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-let wrapper;
+let Dungeon: ShallowWrapper;
 describe("The Dungeon Tile Builder app", () => {
   beforeEach(() => {
     const props = {};
-    wrapper = shallow(<App {...props} />);
+    Dungeon = shallow(<App {...props} />);
   });
   it("should match the snapshot", () => {
-    expect(wrapper).toMatchSnapshot();
+    expect(Dungeon).toMatchSnapshot();
   });
 });
