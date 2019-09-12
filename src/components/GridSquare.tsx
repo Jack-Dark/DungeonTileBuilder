7432f2ff0b6ac6aa03e55d546cf601f245207076
Jack-Dark <decker3d@gmail.com>
2019-09-11 22:40:51 -0500

    chore: Adds alt text to grid placeholders

--- src/components/GridSquare.tsx
+++ src/components/GridSquare.tsx
@@ -5,7 +5,10 @@ export interface GridSquareProps {}
 export default class GridSquare extends React.PureComponent<GridSquareProps> {
   render(): React.ReactNode {
     return (
-      <img src=" data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJAAAACQAQMAAADdiHD7AAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAZQTFRFAAAA////pdmf3QAAAC9JREFUeJxjYMAE9f8h4B+cMSo0KjQqNCo0KoRdiDgwKJw6KjQqNCo0KjQkhDABAHJcQZjMLU5yAAAAAElFTkSuQmCC" />
+      <img
+        alt="square grid"
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJAAAACQAQMAAADdiHD7AAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAZQTFRFAAAA////pdmf3QAAAC9JREFUeJxjYMAE9f8h4B+cMSo0KjQqNCo0KoRdiDgwKJw6KjQqNCo0KjQkhDABAHJcQZjMLU5yAAAAAElFTkSuQmCC"
+      />
     );
   }
 }
