fc15231caa2b97707041de471ab729d11b007d16
Jack-Dark <decker3d@gmail.com>
2019-09-11 22:38:54 -0500

    test: Updates test descriptions.

--- src/__tests__/utils.test.tsx
+++ src/__tests__/utils.test.tsx
@@ -12,14 +12,14 @@ describe('The createCoords function', () => {
   it('should create an array of numbers with the correct values', () => {
     expect(coords).toEqual([-2, -1, 1, 2, 3]);
   });
-  describe('when passed an argument that is an odd number', () => {
+  describe('when the "qty" argument is an odd number', () => {
     it('should create an array where the difference between the first and last index is 1', () => {
       const firstIndex = 0;
       const lastIndex = coords.length - 1;
       expect(coords[firstIndex] + coords[lastIndex]).toBe(1);
     });
   });
-  describe('when passed an argument that is an even number', () => {
+  describe('when the "qty" argument is an even number', () => {
     const numOfCoords = 4;
     beforeEach(() => {
       coords = createCoords(numOfCoords);
