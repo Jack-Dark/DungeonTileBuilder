42ca0a69d8149a13e3874d954f9ecf831bae906b
Jack-Dark <decker3d@gmail.com>
2019-09-11 19:09:39 -0500

    fix: Adds missing css directory update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 // import logo from "./logo.svg";
 import Tile from "./components/Tile";
-import "./App.css";
+import "./css/App.css";
 
 const App: React.FC = () => {
   return <Tile label="foo" value={1} />;
