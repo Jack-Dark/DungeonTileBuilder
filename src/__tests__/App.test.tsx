f50f626ddaa47039d71e6864df48797bc05217f6
Jack-Dark <decker3d@gmail.com>
2019-09-11 22:41:36 -0500

    test: Lints test file.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import App from "../App";
-import { shallow, mount, render, ShallowWrapper } from "enzyme";
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import React from 'react';
+import App from '../App';
+import { shallow, mount, render, ShallowWrapper } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
 let Dungeon: ShallowWrapper;
-describe("The Dungeon Tile Builder app", () => {
+describe('The Dungeon Tile Builder app', () => {
   beforeEach(() => {
     const props = {};
-    Dungeon = shallow(<App {...props} />);
+    Dungeon = mount(<App {...props} />);
   });
-  it("should match the snapshot", () => {
+  it('should match the snapshot', () => {
     expect(Dungeon).toMatchSnapshot();
   });
-  it("should have a Tile component", () => {
-    expect(Dungeon.exists("Tile")).toBeTruthy();
+  it('should have a Tile component', () => {
+    expect(Dungeon.exists('Tile')).toBeTruthy();
   });
 });
