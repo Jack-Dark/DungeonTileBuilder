94535648f573c344942f0a0dddb30fded5c48a82
Jack-Dark <decker3d@gmail.com>
2019-09-12 20:10:44 -0500

    chore: Adds styling to starter grid types.

--- src/components/GridSquare.tsx
+++ src/components/GridSquare.tsx
@@ -6,6 +6,7 @@ export default class GridSquare extends React.PureComponent<GridSquareProps> {
   render(): React.ReactNode {
     return (
       <img
+        className="fl"
         alt="square grid"
         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJAAAACQAQMAAADdiHD7AAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAZQTFRFAAAA////pdmf3QAAAC9JREFUeJxjYMAE9f8h4B+cMSo0KjQqNCo0KoRdiDgwKJw6KjQqNCo0KjQkhDABAHJcQZjMLU5yAAAAAElFTkSuQmCC"
       />
