69f7d141a1384096c024589c2af7d0ca1a72e4ce
Jack-Dark <decker3d@gmail.com>
2019-09-17 21:34:43 -0500

    chore: Updates styling

--- src/components/Controls/ResizeGrid.tsx
+++ src/components/Controls/ResizeGrid.tsx
@@ -8,23 +8,22 @@ export interface ResizeGridProps {
 export default class ResizeGrid extends React.PureComponent<ResizeGridProps> {
     render(): React.ReactNode {
         const { resizeGrid, location } = this.props;
+        const isXAxis = /left|right/.test(location);
         return (
             <div
                 className={[
-                    `absolute z-2 ${location}-0 flex`,
-                    /left|right/.test(location)
-                        ? 'flex-column v-center'
-                        : 'flex-row h-center'
+                    `fixed z-3 ${location}-0 flex`,
+                    isXAxis ? 'flex-column v-center' : 'flex-row h-center'
                 ].join(' ')}
             >
                 <div
-                    className="pa2 pointer"
+                    className={`resize pa2 pointer ${isXAxis ? 'o-1' : 'o-2'}`}
                     onClick={() => resizeGrid(location, '+')}
                 >
                     +
                 </div>
                 <div
-                    className="pa2 pointer"
+                    className={`resize pa2 pointer ${isXAxis ? 'o-2' : 'o-1'}`}
                     onClick={() => resizeGrid(location, '-')}
                 >
                     -
